9a0065844d7d6023ab1bc7e34f07da01e796b5a4
Wilfred Springer <wilfredspringer@gmail.com>
2012-09-23 21:41:01 +0200

    Add some comments

--- src/main/scala/nl/flotsam/lzw/Lzw.scala
+++ src/main/scala/nl/flotsam/lzw/Lzw.scala
@@ -19,6 +19,21 @@ package nl.flotsam.lzw
 
 object Lzw {
 
+  /**
+   * LZW encodes a sequence of bytes. Returns a non-strict collection of tuples, where the first element of the tuple
+   * represents a value to be send to the output, and the second element the minimal number of bits expected to be
+   * used for representing the output.
+   *
+   * Depending on the size of the index, the number of bits used to represent pointers to elements of the index can
+   * vary (and will grow while encoding). There are different policies for dealing with this while writing the output
+   * values. Some may prefer to always allocate a fixed number of bits for the output values,
+   * while others might prefer to limit the number of bits written to the minimum needed. By explicitly passing the
+   * number of bits required *minimally* to store the output value, callers can choose to implement any policy they
+   * deem appropriate.
+   *
+   * @param in A collection of bytes.
+   * @return A non-strict collection providing the values of the LZW encoded representation of the input collection.
+   */
   def encode(in: Traversable[Byte]): Traversable[(Int, Int)] =
     new Traversable[(Int, Int)] {
       def foreach[U](f: ((Int, Int)) => U) {
@@ -28,6 +43,26 @@ object Lzw {
       }
     }
 
+  /**
+   * LZW decodes a sequence of integer numbers. The important thing to remember is that LZW defines a mapping from a
+   * sequence of bytes to pointers to an index - with the index being built on the fly. There is a *direct* relation
+   * between the number of elements in the index, and the number of bits required to represent pointers into that
+   * index. If the number of elements in the index is 512, every pointer requires at least 9 bits.
+   *
+   * However, that does not mean that every pointer is always encoded the minimal number of bits required to make LZW
+   * work. There are different policies that could work. You could for instance implement a scheme that always uses
+   * two bytes. You could gain compression, but loose ease of implementation, and potentially also (some) speed.
+   *
+   * Because of all of this, this method does not assume a particular way in which LZW output values are encoded into
+   * bytes. It leaves that to the callers of the decode operation. The factory accepted creates the Traversable from
+   * which the integer values are read. The factory itself accepts a function that allows the factory to pass a
+   * reference to a function that informs it of the number of bits required at any given moment. Implementations can
+   * choose to ignore this function, and always construct the integer values from the same number of bits. Or they
+   * can use it and leverage the information provided by the decoder.
+   *
+   * @param factory The factory constructing a Traversable of integer values, used as input for the decode operation.
+   * @return A Traversable providing the decoded version of the values passed in.
+   */
   def decode(factory: (() => Int) => Traversable[Int]): Traversable[Byte] = {
     new Traversable[Byte] {
       def foreach[U](f: (Byte) => U) {
